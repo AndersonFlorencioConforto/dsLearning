f16adb22a5331b77fad200ebd479bac6d5e4f4c0
Anderson Conforto <afcaconforto@gmail.com>
2021-09-01 15:18:38 -0300

    Tabela resource criada

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -21,3 +21,13 @@ INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('1.0
 INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('2.0', TIMESTAMP WITH TIME ZONE '2020-12-20T03:00:00Z', TIMESTAMP WITH TIME ZONE '2021-12-20T03:00:00Z', 1);
 
 INSERT INTO tb_notification (text, moment, read, route, user_id) VALUES ('Parabéns', TIMESTAMP WITH TIME ZONE '2020-12-20T03:00:00Z' , TRUE ,'teste', 1);
+
+INSERT INTO tb_resource (title, description, position, img_uri, type, offer_id) VALUES ('Trilha HTML','Trilha principal do curso',1,'teste',1,1);
+INSERT INTO tb_resource (title, description, position, img_uri, type, offer_id) VALUES ('Fórum','Tire suas dúvidas',2,'teste',2,1);
+INSERT INTO tb_resource (title, description, position, img_uri, type, offer_id) VALUES ('Lives','Lives exclusivas para a turma',3,'teste',0,1);
+
+
+
+
+
+
