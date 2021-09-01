b86cf69760349d32f7246e4846d53902775e2090
Anderson Conforto <afcaconforto@gmail.com>
2021-09-01 17:07:16 -0300

    Seed da tabela Lesson, e Fix LessonsDone mapeamento

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -35,6 +35,22 @@ INSERT INTO tb_section (title, description, position, img_uri, resource_id , pre
 INSERT INTO tb_enrollment (user_id,offer_id,enroll_Moment,refund_Moment,available,only_Update) VALUES (1, 1, TIMESTAMP WITH TIME ZONE '2020-11-20T13:00:00Z',null,true,false);
 INSERT INTO tb_enrollment (user_id,offer_id,enroll_Moment,refund_Moment,available,only_Update) VALUES (2, 1, TIMESTAMP WITH TIME ZONE '2020-11-20T13:00:00Z',null,true,false);
 
+INSERT INTO tb_lesson (title, position, section_id) VALUES ('aula 1 do capitulo 1' , 1 ,1);
+INSERT INTO tb_content (id, text_content, video_uri) VALUES (1,'material de apoio', 'teste');
+
+INSERT INTO tb_lesson (title, position, section_id) VALUES ('aula 2 do capitulo 1' , 2 ,1);
+INSERT INTO tb_content (id, text_content, video_uri) VALUES (2,'', 'teste');
+
+INSERT INTO tb_lesson (title, position, section_id) VALUES ('aula 3 do capitulo 1' , 3 ,1);
+INSERT INTO tb_content (id, text_content, video_uri) VALUES (3,'', 'teste');
+
+INSERT INTO tb_lesson (title, position, section_id) VALUES ('tarefa do cap 1' , 4 ,1);
+INSERT INTO tb_task (id, description,question_count,approval_count,weight, due_date) VALUES (4,'fazer tarefa', 5,4,1.0,TIMESTAMP WITH TIME ZONE '2020-11-25T13:00:00Z');
+
+INSERT INTO tb_lessons_done (lesson_id, user_id, offer_id) VALUES (1,1,1);
+INSERT INTO tb_lessons_done (lesson_id, user_id, offer_id) VALUES (2,1,1);
+
+
 
 
 
